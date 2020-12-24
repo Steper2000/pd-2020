066ac81cd6800bcbe40937a82b95329bdd5e0e05
Steper2000 <okunev-85@bk.ru>
2020-12-24 16:01:41 +0300

    изменение html под БД

--- Serv/app/app.py
+++ Serv/app/app.py
@@ -4,25 +4,6 @@ app = Flask(__name__)
 application = app
 
 @app.route('/')
-def aaa():
-    res = None
-    msg = None
-    if request.args:
-        try:
-            n1 = float(request.args.get('n1'))
-            op = request.args.get('op')
-            n2 = float(request.args.get('n2'))
-            if op == '+':
-                res = n1 + n2
-            elif op == '-':
-                res = n1 - n2
-            elif op == '*':
-                res = n1 * n2
-            elif op == '/':
-                res = n1 / n2
-        except ZeroDivisionError:
-            msg = 'You can not devide by zero'
-        except ValueError:
-            msg = 'Please enter numbers only'
-    return render_template('index.html', res = res, msg = msg)
+def index():
+    return render_template('index.html')
 application.run()
