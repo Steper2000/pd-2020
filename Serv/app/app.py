4ac7024fdf338eaa16afead6a9f3659a1a38ef6b
DanilaShmakov <danila.shmakov00@mail.ru>
2020-12-30 22:49:32 +0300

    search

--- Serv/app/app.py
+++ Serv/app/app.py
@@ -4,19 +4,34 @@ import csv
 app = Flask(__name__)
 application = app
 
+with open('test.csv', encoding='utf-8') as csvfile:
+    users = []
+    reader = csv.reader(csvfile)
+    for row in reader:
+        user = {}
+        user["code"] = row[0]
+        user["name"] = row[1]
+        user["birth"] = row[2]
+        user["who"] = row[3]
+        user["pasport"] = row[4]
+        user["account"] = row[5]
+        users.append(user)
+
 @app.route('/')
 def aaa():
-    with open('C:\\Users\\maxim\\Desktop\\PD\\Serv\\app\\test.csv', encoding='utf-8') as csvfile:
-        users = []
-        reader = csv.reader(csvfile)
-        for row in reader:
-            user = {}
-            user["code"] = row[0]
-            user["name"] = row[1]
-            user["birth"] = row[2]
-            user["who"] = row[3]
-            user["pasport"] = row[4]
-            user["account"] = row[5]
-            users.append(user)
+    pass
     return render_template('index.html', users=users)
+
+@app.route('/process_data/', methods=['POST'])
+def search():
+    userssearched= []
+    userssearched.append(users[0])
+    if request.method == 'POST':
+        select = request.form.get('searchoose')
+        information = request.form.get('information') 
+        print(information)
+        # print(next(item for item in users if item['code'] == '000000003'))
+        userssearched.append(next(item for item in users if item[select] == information))
+    return render_template('index.html', users=userssearched)
+
 application.run()
