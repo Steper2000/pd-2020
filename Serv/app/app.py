a742e636927a3816c22dfc06b4d4e5a12604b294
Shadthrough <43291951+Shadthrough@users.noreply.github.com>
2020-12-30 15:27:25 +0300

    added index table view

--- Serv/app/app.py
+++ Serv/app/app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 application = app
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def aaa():
+    with open('C:\\Users\\maxim\\Desktop\\PD\\Serv\\app\\test.csv', encoding='utf-8') as csvfile:
+        users = []
+        reader = csv.reader(csvfile)
+        for row in reader:
+            user = {}
+            user["code"] = row[0]
+            user["name"] = row[1]
+            user["birth"] = row[2]
+            user["who"] = row[3]
+            user["pasport"] = row[4]
+            user["account"] = row[5]
+            users.append(user)
+    return render_template('index.html', users=users)
 application.run()
