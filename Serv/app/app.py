8f20fcb4b02ce29f98ecfb655ae19a9b81976c35
Steper2000 <okunev-85@bk.ru>
2021-01-18 17:19:43 +0300

    mask search added

--- Serv/app/app.py
+++ Serv/app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import csv
+import csv, re
 
 app = Flask(__name__)
 application = app
@@ -19,9 +19,43 @@ with open('test.csv', encoding='utf-8') as csvfile:
     headers=users.pop(0)
 
 @app.route('/')
-def aaa():
+def index():
     pass
     return render_template('index.html', users=users, headers=headers)
+
+@app.route('/pd')
+def pd():
+    mask=''
+    return render_template('pd.html', users=users, headers=headers, mask=mask)
+
+@app.route('/pd', methods=['POST'])
+def pdsearch():
+    mask=''
+    # Подбор маски
+    if request.method == 'POST':
+        select = request.form.get('datatype')
+        if select=="pasport":
+            mask=r'^\d\d\d\d\d\d\d\d\d\d$'
+        elif select=="account":
+            mask=r'^\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d$'
+        elif select=="new":
+            mask=request.form.get('selfmask')
+    # Её приминение
+    pdlist=[]
+    pdkeys=[]
+    for user in users:
+        for key in user:
+            res=re.match(mask, user[key])
+            if res!=None:
+                print(key, user[key], mask, res)
+                pdkeys.append(key)
+                print(pdkeys)
+        print(pdkeys)
+        pdlist.append(pdkeys.copy()) # Оказывается если не написать копи, то в pdlist будут меняться внутренности, когда меняется pdkeys
+        print(pdlist)
+        pdkeys.clear()
+    print(pdlist)
+    return render_template('pd.html', users=users, headers=headers, mask=mask, pdlist=pdlist)
 
 @app.route('/process_data/', methods=['POST'])
 def search():
